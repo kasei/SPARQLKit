f55736428cf64bdbc365ae253f275bfc817958f2
Gregory Todd Williams <greg@evilfunhouse.com>
2013-10-24 15:37:09 -0400

    Added GTWExpressionEvaluationContext class.

---
+++ GTWSPARQLEngine/GTWExpressionEvaluationContext.h
@@ -0,0 +1,13 @@
+//
+//  GTWExpressionEvaluationContext.h
+//  GTWSPARQLEngine
+//
+//  Created by Gregory Williams on 10/24/13.
+//  Copyright (c) 2013 Gregory Williams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTWExpressionEvaluationContext : NSObject
+
+@end
---
+++ GTWSPARQLEngine/GTWExpressionEvaluationContext.m
@@ -0,0 +1,13 @@
+//
+//  GTWExpressionEvaluationContext.m
+//  GTWSPARQLEngine
+//
+//  Created by Gregory Williams on 10/24/13.
+//  Copyright (c) 2013 Gregory Williams. All rights reserved.
+//
+
+#import "GTWExpressionEvaluationContext.h"
+
+@implementation GTWExpressionEvaluationContext
+
+@end
