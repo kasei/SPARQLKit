ee824a811f647b8f49382ef409cf550dfe963e67
Gregory Todd Williams <greg@evilfunhouse.com>
2013-11-18 15:27:12 -0800

    Removed GTWAddressBookTripleStore implementation (split off into separate project).

---
+++ SPARQLKitTests/SPARQLKitTests.m
@@ -0,0 +1,34 @@
+//
+//  SPARQLKitTests.m
+//  SPARQLKitTests
+//
+//  Created by Gregory Williams on 11/18/13.
+//  Copyright (c) 2013 Gregory Williams. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SPARQLKitTests : XCTestCase
+
+@end
+
+@implementation SPARQLKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
